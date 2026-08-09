e72cf5408af3a76c5035f088d24ea4d65535a09f
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-09 23:45:31 +0800

    Create OrientationChoiceActivity.kt

---
+++ app/src/main/java/com/lingmiao/v2/OrientationChoiceActivity.kt
@@ -0,0 +1,8 @@
+<resources>
+    <style name="AppTheme" parent="Theme.AppCompat.Light.DarkActionBar">
+        <item name="colorPrimary">@color/pink_primary</item>
+        <item name="colorPrimaryDark">@color/pink_primary</item>
+        <item name="colorAccent">@color/pink_primary</item>
+        <item name="android:windowBackground">@color/pink_primary_light</item>
+    </style>
+</resources>
